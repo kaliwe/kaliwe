5d5e5ec0aa24e6060d5353b9fea371bc11cdc305
Plotnikov Anton <plotnikovanton@gmail.com>
2016-10-31 02:31:56 +0300

    Precompilation configuration statement

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ require 'coffee-script'
 require 'sass'
 require 'execjs'
 
-
 class App < Sinatra::Base
   set :views, proc { File.join(root, 'views') }
   set :public_folder, File.join(File.dirname(__FILE__), 'public')
 
-  set :assets_paths, %w{assets/javascripts assets/stylesheets
-                        vendor/assets/javascripts}
+  set :assets_precompile, %w(app.js app.css)
+  set :assets_paths, %w(assets/javascripts assets/stylesheets
+                        vendor/assets/javascripts)
   set :assets_css_compressor, :sass
   set :assets_js_compressor, :uglifier
 
